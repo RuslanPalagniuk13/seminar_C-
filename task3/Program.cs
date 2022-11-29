5b2449d589f1d3562bd6d3758bd6f334f5f154c2
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-11-29 16:07:42 +0300

    Решение задачи 3 сем.1

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = new Random().Next(1,8);
+
+if (number == 1) Console.WriteLine ("Понедельник");
+else if (number == 2) Console.WriteLine ("Вторник");
+else if (number == 3) Console.WriteLine ("Среда");
+else if (number == 4) Console.WriteLine ("Четверг");
+else if (number == 5) Console.WriteLine ("Пятница");
+else if (number == 6) Console.WriteLine ("Суббота");
+else if (number == 7) Console.WriteLine ("Воскресенье");
