a2dc63480d17fe25b020b123902525669717e4d0
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-11-29 16:39:22 +0300

    Решение задачи 4 сем.1

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, котоорая на вход принимает одно число, 
+а на выходе выводит все целые числа от -N до N */
+
+Console.Write("Введите число: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+int count = -namber;
+while (count <= namber)
+{
+   Console.Write($"{count}, "); /* Знак $ выводит форматированный вывод */
+   count++;
+}
